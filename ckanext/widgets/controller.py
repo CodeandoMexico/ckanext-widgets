6d61951c956cca6ffbe702dd180cf978199634e9
Noé Domínguez Porras <zeugop@gmail.com>
2014-09-15 03:56:47 -0500

    Revert "New widget layout with image from the organization and new banner or wide widget layout"

--- ckanext/widgets/controller.py
+++ ckanext/widgets/controller.py
@@ -1,15 +1,13 @@
 import ckan.plugins as p
 import logging
-from ckan.plugins import toolkit
+import ckan.logic as logic
 
-NotAuthorized = toolkit.NotAuthorized 
-get_action  = toolkit.get_action
-abort  = toolkit.abort
-_  = toolkit._
-c = toolkit.c
-request = toolkit.request
-NotFound = toolkit.ObjectNotFound
+NotFound = logic.NotFound
+NotAuthorized = logic.NotAuthorized
+get_action = p.toolkit.get_action
 
+_ = p.toolkit._
+c = p.toolkit.c
 
 class WidgetsController(p.toolkit.BaseController):
     controller = 'ckanext.widgets.controller.WidgetsController'
@@ -23,19 +21,14 @@ class WidgetsController(p.toolkit.BaseController):
             #'model': model, 'session': model.Session,
             #'user': c.user or c.author, 'auth_user_obj': c.userobj
         }
+        
         try:
             c.package = get_action('package_show')(context, {'id': id})
             data_dict = {'resource': c.resource, 'package': c.package}
-            if 'widget_type' in request.params:
-              if request.params['widget_type'] == 'wide' :
-                return p.toolkit.render('wide_widget.html', data_dict)
-              else:
-                return p.toolkit.render('widget.html', data_dict)
-            else:
-              return p.toolkit.render('widget.html', data_dict)
         except NotFound:
             abort(404, _('Resource not found'))
         except NotAuthorized:
             abort(401, _('Unauthorized to read resource %s') % id)
-        except:
-            abort(500, _('There was an internal error %s') % id)
+        
+        return p.toolkit.render('widget.html', data_dict)
+
